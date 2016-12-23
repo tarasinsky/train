91c2b2788e76be520aa2fc1d1246ae113ad8eec6
Aleksandr Tarasinsky <a.tarasinsky@gmail.com>
2016-12-23 12:53:42 +0200

    Part 9. Forth attempt

--- test_validation.rb
+++ test_validation.rb
@@ -7,18 +7,6 @@ class Test
 
   validate :val, :presense
   validate :val, :is_type, is_class: String
-
-  def validate!
-    @@validation_rules.each do |rule|
-      puts rule
-      rule.each_pair do |method, params|
-        result = self.class.send method, eval("#{params[:value]}"), eval("#{params[:args]}")
-        #result = self.class.send method, params[:value].send("self"), eval("#{params[:args]}")
-        puts result
-      end
-    end
-  end
-
 end
 
 a = "d"
@@ -26,16 +14,4 @@ a = "d"
 t = Test.new
 t.val = a
 
-t.validate!
-
-#result = t.validate t.val, :presense
-#puts "presense #{result}"
-
-#t.val = "а"
-#name_format = /^[а-я]{1}/i
-#result = t.validate t.val, :format, regex: /^[а-я]{1}/i
-#puts "regex #{result}"
-
-#t.val = 1
-#result = t.validate t.val, :type, is_class: String
-#puts "class #{result}"
+puts t.valid?
--- validation.rb
+++ validation.rb
@@ -1,56 +1,54 @@
 module Validation
   def self.included(base)
-    base.extend(ClassMethods2)
+    base.extend(ClassMethods)
     base.include(InstanceMethods)
   end
 
   module ClassMethods
-    def validate(value, check_type, **args)
-      self.send check_type, value, args
-    end
-
-    def presense(value, args)
-      !(value.nil? || value.empty?)
-    end
-
-    def format(value, args)
-      !(value !~ args[:regex])
-    end
-
-    def is_type(value, args)
-      !(value.is_a?(args[:is_class]))
-    end
-
-    def less_than(value, args)
-      !(value < args[:compare_to])
-    end
-  end
-
-  module ClassMethods2
-    def validate(value, check_type, **args)
+    def validate(attr_name, check_type, **args)
       class_variable_set(:@@validation_rules, []) unless class_variable_defined?(:@@validation_rules)
       rules = class_variable_get(:@@validation_rules)
-      rules << {check_type => {:value => value, :args => args}}
+      rules << { check_type => {:attr_name => attr_name, :args => args} }
       class_variable_set(:@@validation_rules, rules)
-    end
-
-    def presense(value, args)
-      (value.nil? || value.empty?)
-    end
-
-    def format(value, args)
-      (value !~ args[:regex])
-    end
-
-    def is_type(value, args)
-      (value.instance_of?(args[:is_class]))
-    end
-
-    def less_than(value, args)
-      (value < args[:compare_to])
     end
   end
 
   module InstanceMethods
+    def presense(value, args)
+      raise 'No value' if value.nil?
+    end
+
+    def format(value, args)
+      raise "Don't match #{args[:regex]}" if (value !~ args[:regex])
+    end
+
+    def is_type(value, args)
+      raise "Not #{args[:is_class]}" unless (value.instance_of?(args[:is_class]))
+    end
+
+    def less_than(value, args)
+      raise "Not < #{args[:compare_to]}" unless (value < args[:compare_to])
+    end
+
+    def validate!
+      class_ref = self.class
+      return unless class_ref.class_variable_defined?(:@@validation_rules)
+      rules = class_ref.class_variable_get(:@@validation_rules)
+      rules.each do |rule|
+        rule.each do |method, params|
+          value = instance_variable_get("@#{params[:attr_name]}")
+          send method, value, params[:args]
+        end
+      end
+      true
+    end
+
+    def valid?
+      begin
+        validate!
+      rescue
+        false
+      end
+    end
   end
 end
